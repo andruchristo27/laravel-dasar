d65aace997d7ebd3f00f613a9b8735593d5e7b03
andruchristo27 <e41211364@student.polije.ac.id>
2024-09-19 10:01:47 +0700

    Book: Create dan Store

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
@@ -11,5 +12,26 @@ class BookController extends Controller
         $books = Book::paginate(10);
 
         return view('books.index', compact('books'));
+    }
+
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'cover_image' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+            'name' => ['required', 'string', 'max:255'],
+            'author' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string'],
+            'is_published' => ['required', 'boolean'],
+        ]);
+
+        $validatedData['cover_image'] = $request->file('cover_image')->store('images', 'public');
+        Book::create($validatedData);
+
+        return to_route('books.index')->with('success', 'Book created successfully');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/books', [BookController::class, 'index']);
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
